da69f3d4d2005998ab46624571b760901504958a
Liujing <grace_liujing@163.com>
2013-07-17 12:07:18 +0800

    [liujing]plus test

--- WhiteBorad.Tests/PersonControllerFact.cs
+++ WhiteBorad.Tests/PersonControllerFact.cs
@@ -1,9 +1,50 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Web.Mvc;
+using Xunit;
+using WhiteBorad.Models;
+using WhiteBorad.Controllers;
+using WhiteBorad.Repositories;
 
 namespace WhiteBorad.Tests
 {
     public class PersonControllerFact
     {
+        [Fact]
+        public void number_should_be_2_when_person_repo_has_2_items()
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var path = Path.GetFullPath(baseDirectory + "../../../" + "testPerson.xml");
+            var pController = new PersonController(path);
+            var personsFromGetPerson = (IList<Person>)pController.GetPerson();
+            Assert.Equal(2, personsFromGetPerson.Count);
+
+        }
+        [Fact]
+        public void number_should_plus_one_when_add_person_info()
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var path = Path.GetFullPath(baseDirectory + "../../../"+"testPerson.xml");
+            var newPerson = new Person
+                {
+                    Name = "zhangsan",
+                    Age = 30,
+                    Sex = "female",
+                    Hometown = "Sichuan",
+                    Hobby = "playing"
+                };
+            var oldController = new PersonController(path);
+            var oldPersons= (IList<Person>)oldController.GetPerson();     
+            oldController.PostPerson(newPerson);
+            var newController = new PersonController(path);
+            var newPersons= (IList<Person>)newController.GetPerson(); 
+            Assert.Equal(oldPersons.Count+1,newPersons.Count);
+        }
+
 
     }
+
 }
--- WhiteBorad/Controllers/PersonController.cs
+++ WhiteBorad/Controllers/PersonController.cs
@@ -7,90 +7,38 @@ using System.Web.Hosting;
 using System.Web.Http;
 using System.Xml;
 using WhiteBorad.Models;
+using WhiteBorad.Repositories;
 
 namespace WhiteBorad.Controllers
 {
     public class PersonController : ApiController
     {
-        private List<Person> people = new List<Person>();
-        public string Filename = HostingEnvironment.MapPath("~/person.xml");
+        
+        public readonly IPersonRepository personRepo;
+        public PersonController()
+        {
+            string mapPath = HostingEnvironment.MapPath("~/person.xml");
+            personRepo = new XmlPersonRepository(mapPath);
+        }
+
+        public PersonController(string mapPath)
+        {
+            personRepo = new XmlPersonRepository(mapPath);
+        }
+
+        public PersonController(IPersonRepository personRepo)
+        {
+            this.personRepo = personRepo;
+        }
 
         public Person PostPerson(Person newPerson)
         {
-            var person = new Person
-                {
-                    Name = newPerson.Name,
-                    Age = newPerson.Age,
-                    Hometown = newPerson.Hometown,
-                    Sex = newPerson.Sex,
-                    Hobby = newPerson.Hobby
-                };
-            var xmlDoc = new XmlDocument();
-            xmlDoc.Load(Filename); 
-            var root = xmlDoc.SelectSingleNode("person");
-            var xe1=xmlDoc.CreateElement("member");    
-            var xesub1=xmlDoc.CreateElement("name");  
-            xesub1.InnerText=newPerson.Name;   
-            xe1.AppendChild(xesub1);  
-            var xesub2=xmlDoc.CreateElement("age");  
-            xesub2.InnerText=newPerson.Age.ToString();  
-            xe1.AppendChild(xesub2);
-            var xesub3 = xmlDoc.CreateElement("sex");
-            xesub3.InnerText = newPerson.Sex;
-            xe1.AppendChild(xesub3);
-            var xesub4=xmlDoc.CreateElement("hometown");  
-            xesub4.InnerText=newPerson.Hometown;  
-            xe1.AppendChild(xesub4);
-            var xesub5 = xmlDoc.CreateElement("hobby");
-            xesub5.InnerText = newPerson.Hobby;
-            xe1.AppendChild(xesub5);
-            if (root != null) 
-                root.AppendChild(xe1); 
-            xmlDoc.Save(Filename);
-            return person;
+            return personRepo.Add(newPerson);
         }
 
         public IEnumerable<Person> GetPerson()
         {
-            var xmlDoc = new XmlDocument();
-            xmlDoc.Load(Filename);
-            var xn=xmlDoc.SelectSingleNode("person");
-            if (xn != null)
-            { 
-                var xnChild=xn.ChildNodes;     
-                foreach(XmlNode xnChildTemp in xnChild)//xnf is a member  
-                {   var person = new Person();
-                    XmlElement xe=(XmlElement)xnChildTemp;      
-                    XmlNodeList xnList=xe.ChildNodes;    
-                    foreach(XmlNode xnListTemp in xnList)   
-                    {
-                        var xe2 = (XmlElement)xnListTemp;   
-                        if(xe2.Name=="name")    
-                        {
-                            person.Name = xe2.InnerText;
-                        }
-                        else if (xe2.Name=="age")
-                        {
-                            person.Age = Convert.ToUInt16(xe2.InnerText);
-                        }
-                        else if (xe2.Name == "sex")
-                        {
-                            person.Sex = xe2.InnerText;
-                        }
-                        else if (xe2.Name == "hometown")
-                        {
-                            person.Hometown = xe2.InnerText;
-                        }
-                        else
-                        {
-                            person.Hobby = xe2.InnerText;
-                        }
-                    }
-                    people.Add(person);
-                }
-            }
-
-            return people.ToList();
+            return personRepo.LoadAllPersons();
         }
     }
 }
