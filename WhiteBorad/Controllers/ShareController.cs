a965f521a28c8a4d941db74df5eb4891deb85439
githubch <944214536@qq.com>
2013-07-24 18:31:18 +0800

    [ch for sharing]

---
+++ WhiteBorad/Controllers/ShareController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WhiteBorad.Models;
+
+namespace WhiteBorad.Controllers
+{
+    public class ShareController : ApiController
+    {
+        private static readonly List<Sharing> Sharecontent = new List<Sharing>();
+        public IEnumerable<string> Get()
+        {
+            return Sharecontent.Select(name => name.Name).ToArray();
+        }
+
+        public void Put(Sharing input)
+        {
+            if (Sharecontent.Any(item => item.Name.Equals(input.Name)))
+            {
+                return;
+            }
+            Sharecontent.Add(input);
+        }
+
+        public HttpResponseMessage Post(Sharing newInput)
+        {
+            if (string.IsNullOrEmpty(newInput.Name))
+            {
+                return null;// return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            Sharecontent.Add(newInput);
+
+            var httpResponseMessage = Request.CreateResponse(HttpStatusCode.Created);
+            //httpResponseMessage.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = Sharecontent.Count }));
+            return httpResponseMessage;
+        }
+
+    }
+}
---
+++ WhiteBorad/Scripts/my-share.js
@@ -0,0 +1,24 @@
+﻿function ShareController($scope, $http) {
+    $scope.add = function () {
+        //var input = {Name: "test"};
+        //console.log($scope.share);
+        var input = { Name: $scope.share };
+        $http.post('/api/Share', input).success(function (data, status, headers, config) {
+            //$scope.share = "sucess"; // assign  $scope.persons here as promise is resolved here
+            //$scope.share = "";
+            $http.get('/api/Share').success(function (data) {
+                //console.log("dta2: ", data);
+                $scope.Shared = data;
+            });
+        }).error(function (data, status, headers, config) {
+            $scope.share = "error";
+        });
+    };
+    $scope.show = function () {
+        $http.get('/api/Share').success(function (data) {
+            //console.log("dta2: ", data);
+            //$scope.Shared = "The share is:";
+            $scope.Shared = data;
+        });
+    };
+};
