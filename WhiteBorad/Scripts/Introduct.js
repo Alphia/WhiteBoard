4e7372386a465655997337187f89631d3293807e
grace-liujing <graceliu@thoughtworks.com>
2013-08-07 08:58:08 +0800

    [liujing] check the box

--- WhiteBorad/Scripts/Introduct.js
+++ WhiteBorad/Scripts/Introduct.js
@@ -6,8 +6,12 @@
     $scope.AddAction = function () {
         var person = {
             "Name": $scope.name,
-            "Description": $scope.description,
+            "Description": $scope.description
         };
+        if ($scope.name == null || $scope.description == null) {
+            alert("fill the blanks");
+            return;
+        }
         $http.post('api/person', person).success(function () {
             $scope.name = "";
             $scope.description = "";
@@ -15,11 +19,11 @@
         });
     };
 
-    $scope.Edit = function(person) {
+    $scope.Edit = function (person) {
         person.mode = 'edit';
     };
 
-    $scope.Save = function(person) {
+    $scope.Save = function (person) {
         $http.put('api/person', person).success(function () {
             person.mode = 'show';
         });
